9884b2195ed866c4a9afdbdc4aff19d83df801df
anthonyanader <anthonyanader@gmail.com>
2018-01-09 12:16:18 -0500

    first commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Anthony A. Nader on 2018-01-09.
+//  Copyright © 2018 Anthony A. Nader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
