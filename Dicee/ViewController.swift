68fd19014b618a4435878bab3deaabe4a5f5164e
anthonyanader <anthonyanader@gmail.com>
2018-01-11 17:30:22 -0500

    added dice images to array. Configured roll button to generate two random numbers for dice image index

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,13 +13,15 @@ class ViewController: UIViewController {
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
-    @IBOutlet weak var diceImageView1: UIImageView!
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     
+    @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +30,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollDiceButton(_ sender: UIButton) {
+        updateDiceImages()
         
+    }
+    
+    func updateDiceImages() -> Void {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
-        print(randomDiceIndex2)
+        diceImageView1.image = UIImage (named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage (named: diceArray[randomDiceIndex2])
+    
     }
-
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
